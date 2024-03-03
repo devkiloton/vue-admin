b9afe6ded6055df2d33fd278a36c1ec9ee7727fe
devkiloton <dev.kiloton@gmail.com>
2024-03-03 20:55:24 -0300

    Setting up withCredentials to true

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
 
 loadFonts();
 
