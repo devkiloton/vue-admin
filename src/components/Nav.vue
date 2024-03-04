be29b5ebc3195975ae953b63f338eadd269519bd
devkiloton <dev.kiloton@gmail.com>
2024-03-03 23:39:17 -0300

    Creating menu and nav component

---
+++ src/components/Nav.vue
@@ -0,0 +1,35 @@
+<template>
+  <header
+    class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow"
+  >
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#"
+      >Company name</a
+    >
+
+    <nav class="my-2 my-md-0 mr-md-3">
+      <RouterLink to="/profile" class="p-2 text-white text-decoration-none">{{
+        user?.first_name + " " + user?.last_name
+      }}</RouterLink>
+      <a href="#" class="p-2 text-white text-decoration-none" @click="logout"
+        >Sign out</a
+      >
+    </nav>
+  </header>
+</template>
+
+<script>
+import { apiClient } from "@/clients";
+import { RouterLink } from "vue-router";
+
+export default {
+  name: "Nav",
+  methods: {
+    async logout() {
+      await apiClient.auth.logout();
+
+      await this.$router.push("/login");
+    },
+  },
+  props: ["user"],
+};
+</script>
