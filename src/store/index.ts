6835f257555bbc76ece00ccc0fd3a582ebd921ca
devkiloton <dev.kiloton@gmail.com>
2024-03-05 02:26:56 -0300

    Creating action to set user

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,18 @@
+import { User } from "@/models/user";
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    user: new User(),
+  },
+  mutations: {
+    SET_USER(state, user: User) {
+      state.user = user;
+    },
+  },
+  actions: {
+    setUser({ commit }, user: User) {
+      commit("SET_USER", user);
+    },
+  },
 });
